9857a7179b073e7c459d1e926d8021c92a95ca24
denis-bush <denis.bush98@gmail.com>
2022-04-15 16:41:08 +0300

    first commit

---
+++ 5 Exp/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace _5_Exp
+{
+    partial class ExpCalc
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lab_roundval = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.but_calculate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lab_initial = new System.Windows.Forms.Label();
+            this.lab_accuracy = new System.Windows.Forms.Label();
+            this.lab_summs = new System.Windows.Forms.Label();
+            this.lab_accurval = new System.Windows.Forms.Label();
+            this.but_close = new System.Windows.Forms.Button();
+            this.but_reset = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "exp(x) = 1 + x/1! + x^2/2! + x^3/3! + ...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 298);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Точное значение exp(x) =";
+            // 
+            // lab_roundval
+            // 
+            this.lab_roundval.AutoSize = true;
+            this.lab_roundval.Location = new System.Drawing.Point(166, 298);
+            this.lab_roundval.Name = "lab_roundval";
+            this.lab_roundval.Size = new System.Drawing.Size(0, 13);
+            this.lab_roundval.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.radioButton1.Location = new System.Drawing.Point(14, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(40, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "0.1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.radioButton2.Location = new System.Drawing.Point(14, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(46, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "0.01";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.radioButton3.Location = new System.Drawing.Point(93, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(52, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "0.001";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.radioButton4.Location = new System.Drawing.Point(93, 42);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(58, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.Text = "0.0001";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.radioButton5.Location = new System.Drawing.Point(184, 19);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(64, 17);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.Text = "0.00001";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.radioButton6.Location = new System.Drawing.Point(184, 42);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(70, 17);
+            this.radioButton6.TabIndex = 5;
+            this.radioButton6.Text = "0.000001";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox2.Location = new System.Drawing.Point(12, 105);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(272, 69);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Выбор точности";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(12, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(272, 57);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ввод значения X (-13 <= X <= 20)";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(38, 22);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(222, 20);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "1";
+            this.textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(11, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "x =";
+            // 
+            // but_calculate
+            // 
+            this.but_calculate.Location = new System.Drawing.Point(12, 187);
+            this.but_calculate.Name = "but_calculate";
+            this.but_calculate.Size = new System.Drawing.Size(272, 23);
+            this.but_calculate.TabIndex = 5;
+            this.but_calculate.Text = "Расчёт exp(x)";
+            this.but_calculate.UseVisualStyleBackColor = true;
+            this.but_calculate.Click += new System.EventHandler(this.but_calculate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(48, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(198, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Результаты расчёта:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Исходное значение =";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 276);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Точность =";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 320);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Число слагаемых =";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 342);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(108, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Сумма ряда exp(x) =";
+            // 
+            // lab_initial
+            // 
+            this.lab_initial.AutoSize = true;
+            this.lab_initial.Location = new System.Drawing.Point(166, 254);
+            this.lab_initial.Name = "lab_initial";
+            this.lab_initial.Size = new System.Drawing.Size(0, 13);
+            this.lab_initial.TabIndex = 11;
+            // 
+            // lab_accuracy
+            // 
+            this.lab_accuracy.AutoSize = true;
+            this.lab_accuracy.Location = new System.Drawing.Point(166, 276);
+            this.lab_accuracy.Name = "lab_accuracy";
+            this.lab_accuracy.Size = new System.Drawing.Size(22, 13);
+            this.lab_accuracy.TabIndex = 12;
+            this.lab_accuracy.Text = "0.1";
+            // 
+            // lab_summs
+            // 
+            this.lab_summs.AutoSize = true;
+            this.lab_summs.Location = new System.Drawing.Point(166, 320);
+            this.lab_summs.Name = "lab_summs";
+            this.lab_summs.Size = new System.Drawing.Size(0, 13);
+            this.lab_summs.TabIndex = 13;
+            // 
+            // lab_accurval
+            // 
+            this.lab_accurval.AutoSize = true;
+            this.lab_accurval.Location = new System.Drawing.Point(166, 342);
+            this.lab_accurval.Name = "lab_accurval";
+            this.lab_accurval.Size = new System.Drawing.Size(0, 13);
+            this.lab_accurval.TabIndex = 14;
+            // 
+            // but_close
+            // 
+            this.but_close.Location = new System.Drawing.Point(196, 367);
+            this.but_close.Name = "but_close";
+            this.but_close.Size = new System.Drawing.Size(88, 23);
+            this.but_close.TabIndex = 15;
+            this.but_close.Text = "Закрыть";
+            this.but_close.UseVisualStyleBackColor = true;
+            this.but_close.Click += new System.EventHandler(this.but_close_Click);
+            // 
+            // but_reset
+            // 
+            this.but_reset.Location = new System.Drawing.Point(102, 367);
+            this.but_reset.Name = "but_reset";
+            this.but_reset.Size = new System.Drawing.Size(88, 23);
+            this.but_reset.TabIndex = 16;
+            this.but_reset.Text = "Сброс";
+            this.but_reset.UseVisualStyleBackColor = true;
+            this.but_reset.Click += new System.EventHandler(this.but_reset_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 2000;
+            this.toolTip1.InitialDelay = 500;
+            this.toolTip1.ReshowDelay = 100;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            this.toolTip1.ToolTipTitle = "Предупреждение";
+            // 
+            // ExpCalc
+            // 
+            this.AcceptButton = this.but_calculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 404);
+            this.Controls.Add(this.but_reset);
+            this.Controls.Add(this.but_close);
+            this.Controls.Add(this.lab_accurval);
+            this.Controls.Add(this.lab_summs);
+            this.Controls.Add(this.lab_accuracy);
+            this.Controls.Add(this.lab_initial);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.but_calculate);
+            this.Controls.Add(this.lab_roundval);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "ExpCalc";
+            this.Text = "Расчёт exp(x)";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lab_roundval;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button but_calculate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lab_initial;
+        private System.Windows.Forms.Label lab_accuracy;
+        private System.Windows.Forms.Label lab_summs;
+        private System.Windows.Forms.Label lab_accurval;
+        private System.Windows.Forms.Button but_close;
+        private System.Windows.Forms.Button but_reset;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ 5 Exp/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5_Exp
+{
+    public partial class ExpCalc : Form
+    {
+        public ExpCalc()
+        {
+            InitializeComponent();
+        }
+
+        // Закрыть программу
+        private void but_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Очистить все поля
+        private void but_reset_Click(object sender, EventArgs e)
+        {
+            lab_initial.Text = "";
+            lab_accuracy.Text = "";
+            lab_roundval.Text = "";
+            lab_summs.Text = "";
+            lab_accurval.Text = "";
+            textBox.Text = "";
+            radioButton1.Checked = true;
+        }
+
+        // Фильтрация вводимых данных
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+            {
+                return;
+            }
+            else
+            {
+                if ((e.KeyChar != '-') && !(Char.IsControl(e.KeyChar)))
+                {
+
+                    toolTip1.Show("Недопустимый символ", textBox, 58, 20, 2000);
+                }
+            }
+            if (e.KeyChar == '-')
+            {
+                if (textBox.Text.IndexOf('-') != -1)
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                    but_calculate.Focus();
+                return;
+            } e.Handled = true;
+        }
+
+        // Вычислить экспоненту
+        private void but_calculate_Click(object sender, EventArgs e)
+        {
+            double exp = 1, temp = 1, x, round_acc_num = 0.1;
+            int round_acc, i = 1;
+            string acc;
+            if ((textBox.Text == "") || (Convert.ToInt16(textBox.Text) < -13) || 
+                (Convert.ToInt16(textBox.Text) > 20))
+            {
+                toolTip1.Show("Введите корректное значение X", textBox, 58, 20, 2000);
+                return;
+            }
+            acc = lab_accuracy.Text;
+            // Выбор точности округления
+            switch (acc)
+            {
+                case "0.1":
+                    round_acc = 1;
+                    round_acc_num = 0.1;
+                    break;
+                case "0.01":
+                    round_acc = 2;
+                    round_acc_num = 0.01;
+                    break;
+                case "0.001":
+                    round_acc = 3;
+                    round_acc_num = 0.001;
+                    break;
+                case "0.0001":
+                    round_acc = 4;
+                    round_acc_num = 0.0001;
+                    break;
+                case "0.00001":
+                    round_acc = 5;
+                    round_acc_num = 0.00001;
+                    break;
+                case "0.000001":
+                    round_acc_num = 0.000001;
+                    round_acc = 6;
+                    break;
+                default: 
+                    round_acc = 1;
+                    break;
+            }
+            x = Convert.ToInt16(textBox.Text);
+            do
+            {
+                temp = temp * x / i;
+                exp += temp;
+                i++;
+            } while (Math.Abs(temp) >= round_acc_num);
+
+            lab_initial.Text = textBox.Text;
+            lab_accurval.Text = Convert.ToString(Math.Round(exp, round_acc + 1));
+            lab_roundval.Text = Convert.ToString(Math.Round(System.Math.Exp(x), round_acc + 1));
+            lab_summs.Text = Convert.ToString(i);
+        }
+
+        // Очистка полей при переключении точности
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            lab_accuracy.Text = radioButton1.Text;
+            lab_initial.Text = "";
+            lab_roundval.Text = "";
+            lab_summs.Text = "";
+            lab_accurval.Text = "";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            lab_accuracy.Text = radioButton2.Text;
+            lab_initial.Text = "";
+            lab_roundval.Text = "";
+            lab_summs.Text = "";
+            lab_accurval.Text = "";
+        }
+        
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            lab_accuracy.Text = radioButton3.Text;
+            lab_initial.Text = "";
+            lab_roundval.Text = "";
+            lab_summs.Text = "";
+            lab_accurval.Text = "";
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            lab_accuracy.Text = radioButton4.Text;
+            lab_initial.Text = "";
+            lab_roundval.Text = "";
+            lab_summs.Text = "";
+            lab_accurval.Text = "";
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            lab_accuracy.Text = radioButton5.Text;
+            lab_initial.Text = "";
+            lab_roundval.Text = "";
+            lab_summs.Text = "";
+            lab_accurval.Text = "";
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            lab_accuracy.Text = radioButton6.Text;
+            lab_initial.Text = "";
+            lab_roundval.Text = "";
+            lab_summs.Text = "";
+            lab_accurval.Text = "";
+        }
+
+    }
+}
